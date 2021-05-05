1b2c82e2753f7196f53a26f15c7b11e8389d8c47
Aliaksandr Senkevich <puskin1914@gmail.com>
2021-05-05 10:19:23 +0300

    add start files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
-const App = () => {
-  return <div className="App">Hello world</div>;
-};
+// image for the pokemon
+// https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png
 
-export default App;
+const pokemon = [
+  { id: 4, name: "charizard" },
+  { id: 10, name: "caterpie" },
+  { id: 77, name: "ponyta" },
+  { id: 108, name: "lickitung" },
+  { id: 132, name: "ditto" },
+  { id: 133, name: "eevee" },
+];
+
+export default function App() {
+  return <div className="app"></div>;
+}
